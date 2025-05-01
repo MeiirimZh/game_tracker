a83bb929ec87b6de226b8ebfcab21a2cbfd37518
MeiirimZh <zh.meyirim@gmail.com>
2025-05-01 18:51:06 +0500

    started working on header

--- index.php
+++ index.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game Tracker</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
 </head>
 <body>
+    <?php
+        require_once "blocks/header.html";
+    ?>
 </body>
 </html>
